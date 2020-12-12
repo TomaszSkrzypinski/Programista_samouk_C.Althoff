ee7800e22532c838142d52936e548d4f6c6481eb
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-12-12 15:36:04 +0100

    wyzwania_rozdzial_3

---
+++ rozdział_3/wyzwania.py
@@ -0,0 +1,38 @@
+# wyświtl 3 różne łańcuchy znaków
+print("łańcuch znaków 1")
+
+print('łańcuch znaków 2')
+
+print('ostatni łańcuch znaków')
+
+
+# wyświetlanie 2 różnych komunikatów w zależności od wartości zmiennej
+
+x = 14
+
+if x <10:
+    print('wartość zmiennej jest mniejsz od 10')
+else:
+    print('wartość zmiennej jest większa lub równa 10')
+
+
+# wyświetlanie 3 różnych komunikatów w zależności od wartości zmiennej
+
+y = 6
+
+if y <= 10:
+    print('wartość zmiennej jest mniejsz lub równa 10')
+elif y > 10 and y <= 25:
+    print('wartość zmiennej jest większa od 10 i mniejsza lub równa 25')
+else:
+    print('wartość zmiennej jest większa od 25')
+
+
+# napisz program dzielący 2 zmienne i wyświetl resztę z dzielenia
+
+print(x % y)
+
+
+# napisz program dzielący 2 zmienne i wyświetl iloraz
+
+print(x / y)
