274ba9f50ae46260cb610416695494c74e4474da
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-12-25 22:02:52 +0100

    rozdzial_10_wyzwania

---
+++ rozdział_10_gra_w_wisielca/wyzwania.py
@@ -0,0 +1,51 @@
+import random
+
+# zmodyfikować wisielca tak, aby odgadywane słowo było słowem losowo wybieranym z podanej listy słów
+
+words = ["słoń", "koń", "kot", "kura", "pies", "żyrafa", "zebra"]
+
+length = len(words)
+
+index = random.randint(0, length - 1)
+
+word = words[index]
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "________         ",
+              "|                ",
+              "|       |        ",
+              "|       0        ",
+              "|      /|\       ",
+              "|      / \       ",
+              "|                ",
+               ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Gra w wisielca")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Odgadnij literę: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "_" not in board:
+            print("Wygrałeś")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("Przegrałeś! Miałeś odgadnąć: {}.".format(word))
+
+hangman(word)
